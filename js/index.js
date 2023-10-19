007da2001b8da618542e35291ed1e6bfa68159bd
aizhuxue007 <aizhuprogram@gmail.com>
2023-10-19 17:06:15 -0400

    animation and navtoggle fixes

--- js/index.js
+++ js/index.js
@@ -5,3 +5,12 @@ AOS.init({
     duration: 1000,
     offset: 100,
 })
+
+const navToggle = document.getElementById('navToggle');
+
+const mobileNav = document.querySelector('.nav__mobile');
+
+navToggle.addEventListener('click', () => {
+    mobileNav.classList.toggle('nav--visible');
+    console.log(mobileNav)
+})
